365b2a9fd56f7c1b170d324204d4e7bfc1a9d2b1
balar2207 <135403084+balar2207@users.noreply.github.com>
2025-03-22 22:16:23 +0530

    yumrhel9.sh

--- yumrhel9.sh
+++ yumrhel9.sh
@@ -9,7 +9,7 @@ cat > /etc/yum.repos.d/rhel9-local.repo <<EOF
 [Local-BaseOS]
 name=Red Hat Enterprise Linux 9 - BaseOS
 metadata_expire=-1
-gpgcheck=1
+gpgcheck=0
 enabled=1
 baseurl=file:///var/www/html/repos/rhel9/BaseOS/
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
@@ -17,7 +17,7 @@ gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
 [Local-AppStream]
 name=Red Hat Enterprise Linux 9 - AppStream
 metadata_expire=-1
-gpgcheck=1
+gpgcheck=0
 enabled=1
 baseurl=file:///var/www/html/repos/rhel9/AppStream/
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
